a5a531ceb43deecc09e8401d5c417f061b5cabd8
leventex1 <angyal_2001@hotmail.hu>
2025-10-15 21:55:59 +0200

    added rickroll ascii

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import { useEffect, useState } from "react"
+import "./App.css"
+import frames from "./modules/ascii-rickroll-js/script"
+
+
+const frameRate = 7
 
 function App() {
+    const [frameIndex, setFrameIndex] = useState<number>(0)
+
+    useEffect(() => {
+        setInterval(() => {
+            setFrameIndex(prev => (prev + 1) % frames.length)
+            console.log("next frame")
+        }, 1000 / frameRate)
+    }, [])
     
     return (
-        <div>
-            rickrolled
+        <div className="screen-container">
+            <h1>You Just Got Rickrolled</h1>
+            <pre>
+                {frames[frameIndex]}
+            </pre>
         </div>
     )
 }
